8cb5410547718a88411225e26ec8b88f78f2d9ef
m4c1elz <milaelipe.maciel@gmail.com>
2024-11-08 10:56:05 -0300

    feat(hirings): add filters

--- server/src/controllers/common-user/get-hirings.ts
+++ server/src/controllers/common-user/get-hirings.ts
@@ -1,19 +1,51 @@
 import { Request, Response } from "express"
 import { prisma } from "@/lib/prisma"
+import { $Enums } from "@prisma/client"
 
-export async function getHirings(req: Request, res: Response) {
+interface HiringFilters {
+    page?: string
+    search?: string
+    status?: $Enums.Status
+}
+
+export async function getHirings(
+    req: Request<{}, {}, {}, HiringFilters>,
+    res: Response
+) {
+    const { page, search, status } = req.query
+
+    let hiringsPage: number
+    if (isNaN(Number(page))) {
+        hiringsPage = 1
+    } else {
+        hiringsPage = Number(page)
+    }
+
     const hirings = await prisma.contratacao.findMany({
         select: {
+            usuarioProfissional: {
+                select: {
+                    idProfissional: true,
+                    nome: true,
+                },
+            },
             dataContratacao: true,
             prazoContratacao: true,
-            precoContratacao: true,
-            usuarioProfissional: true,
+            statusContratacao: true,
         },
         where: {
+            usuarioProfissional: {
+                nome: {
+                    contains: search,
+                },
+            },
             usuarioComum: {
                 idUsuarioComum: req.user.id,
             },
+            statusContratacao: status,
         },
+        take: 8,
+        skip: (hiringsPage - 1) * 8,
     })
 
     res.json(hirings)
