66dac58ce1eeac828e09e8d3413c20b6a75d5242
m4c1elz <milaelipe.maciel@gmail.com>
2024-11-08 09:40:51 -0300

    feat(auth): add verifyToken middleware

--- server/src/middlewares/verify-token.ts
+++ server/src/middlewares/verify-token.ts
@@ -1,4 +1,21 @@
 import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import { env } from "@/env"
+import { UserPayload } from "@/types/user-payload"
 
-// todo
-export function verifyToken(req: Request, res: Response, next: NextFunction) {}
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies["_id"]
+    if (!token) {
+        res.sendStatus(401)
+        return
+    }
+
+    try {
+        const verifiedToken = jwt.verify(token, env.JWT_SECRET) as UserPayload
+        req.user = verifiedToken
+        next()
+    } catch (error) {
+        res.sendStatus(403)
+        return
+    }
+}
---
+++ server/src/types/user-payload.ts
@@ -0,0 +1,5 @@
+export interface UserPayload {
+    id: string
+    usuario: string
+    email: string
+}
