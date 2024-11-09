d583b27c03f3e96e631fbe56ee4e2bb640bba937
m4c1elz <milaelipe.maciel@gmail.com>
2024-11-09 14:02:49 -0300

    refactor(cors): add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 import { commonUserRoutes } from "@/routes/common-user-routes"
 import { authRoutes } from "@/routes/auth-routes"
 import { errorHandler } from "@/middlewares/error-handler"
+import cors from "cors"
 
 const app = express()
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 8080
 // middlewares
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 app.use(authRoutes)
 app.use(commonUserRoutes)
