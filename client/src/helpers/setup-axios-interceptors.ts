997010f484d9afb89c68c7d01480cb44938fd3e4
m4c1elz <milaelipe.maciel@gmail.com>
2024-11-11 18:34:19 -0300

    fix(auth): fix auth interceptor

--- client/src/helpers/setup-axios-interceptors.ts
+++ client/src/helpers/setup-axios-interceptors.ts
@@ -5,12 +5,18 @@ export function setupAxiosInterceptors(axios: AxiosInstance) {
         response => response,
         async (err: AxiosError) => {
             const prevRequest = err.config as InternalAxiosRequestConfig
-            const response = await axios.get("/auth/refresh")
-            if (response.status !== 200) {
+
+            try {
+                if (prevRequest.url === "/auth/refresh") {
+                    throw new Error()
+                }
+
+                await axios.get("/auth/refresh")
+                return axios(prevRequest)
+            } catch (error) {
                 window.location.href = "/login"
-                return
+                return Promise.reject(error)
             }
-            return axios(prevRequest)
         },
     )
 }
