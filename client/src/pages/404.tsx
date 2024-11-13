2cf12e951e4c9eae96e8cbe98de480bdd9e97f04
m4c1elz <milaelipe.maciel@gmail.com>
2024-11-13 11:51:48 -0300

    fix(404): add tabs to 404 page

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,14 +1,16 @@
 import { Sidebar } from "@/components/sidebar"
+import { Tabs } from "@/components/tabs"
 import { Link } from "@/router"
 
 export default function NotFound() {
     return (
         <div className="flex">
             <Sidebar />
-            <div className="m-auto text-center space-y-4">
+            <Tabs />
+            <div className="m-auto space-y-4 py-16 text-center md:py-0">
                 <h1 className="text-destructive">Erro 404</h1>
                 <h3>Essa página não existe.</h3>
-                <Link to="/history" className="underline text-accent">
+                <Link to="/history" className="text-accent underline">
                     Voltar ao início
                 </Link>
             </div>
