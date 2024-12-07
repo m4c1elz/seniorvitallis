7e47e0dd52ef0e0ddf4b9b2ee9419cd9308655cc
m4c1elz <milaelipe.maciel@gmail.com>
2024-12-07 12:25:29 -0300

    fix: add user type checking redirects

--- client/src/components/redirects.tsx
+++ client/src/components/redirects.tsx
@@ -3,6 +3,8 @@ import { Navigate, useLocation } from "react-router-dom"
 import { useAuth } from "@/providers/auth-provider"
 import { Path } from "@/router"
 import { PropsWithChildren } from "react"
+import { isUserCommon } from "@/helpers/is-user-common"
+import { isUserProfessional } from "@/helpers/is-user-professional"
 
 const PRIVATE: Path[] = [
     "/history",
@@ -12,10 +14,17 @@ const PRIVATE: Path[] = [
 ]
 const PUBLIC: Path[] = ["/login"]
 
+const COMMON_ROUTES: Path[] = [
+    "/history",
+    "/professionals",
+    "/professionals/:id",
+]
+const PROFESSIONAL_ROUTES: Path[] = ["/requests"]
+
 interface RedirectsProps extends PropsWithChildren {}
 
 export function Redirects({ children }: RedirectsProps) {
-    const { isAuth } = useAuth()
+    const { isAuth, user } = useAuth()
     const { pathname } = useLocation()
 
     if (pathname == "/") return <Navigate to="/login" />
@@ -25,8 +34,40 @@ export function Redirects({ children }: RedirectsProps) {
     const unAuthenticatedOnPrivatePath =
         !isAuth && PRIVATE.includes(pathname as Path)
 
-    if (authenticatedOnPublicPath) return <Navigate to="/history" replace />
+    const authenticatedOnPrivatePath =
+        isAuth && PRIVATE.includes(pathname as Path)
+
+    const authenticatedOnProfessionalRoute =
+        isAuth && PROFESSIONAL_ROUTES.includes(pathname as Path)
+    const authenticatedOnCommonRoute =
+        isAuth && COMMON_ROUTES.includes(pathname as Path)
+
+    if (authenticatedOnPublicPath) {
+        console.log("authenticated on public path")
+        if (isUserCommon(user)) {
+            return <Navigate to="/history" replace />
+        }
+
+        if (isUserProfessional(user)) {
+            return <Navigate to="/requests" />
+        }
+    }
+
+    if (authenticatedOnPrivatePath) {
+        console.log("auth in private path.")
+        if (isUserCommon(user) && authenticatedOnProfessionalRoute) {
+            console.log("user is professional.")
+            return <Navigate to="/history" replace />
+        }
+
+        if (isUserProfessional(user) && authenticatedOnCommonRoute) {
+            console.log("user is professional.")
+            return <Navigate to="/requests" replace />
+        }
+    }
+
     if (unAuthenticatedOnPrivatePath) return <Navigate to="/login" replace />
+    console.log("unauthenticated")
 
     return children
 }
