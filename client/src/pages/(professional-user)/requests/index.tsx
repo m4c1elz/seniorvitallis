a154bd69c7b3b42254f7d988588a4e09e289e7d2
m4c1elz <milaelipe.maciel@gmail.com>
2024-12-07 12:25:57 -0300

    fix: add requests page route

--- client/src/pages/(professional-user)/requests/index.tsx
+++ client/src/pages/(professional-user)/requests/index.tsx
@@ -1,7 +1,9 @@
 import { api } from "@/lib/api"
 import { useQuery } from "@tanstack/react-query"
 import { ContractRequests } from "./types"
-import { Calendar } from "lucide-react"
+import { Calendar, CalendarDays, CalendarX } from "lucide-react"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 export default function Requests() {
     const { data: requests, isPending } = useQuery<ContractRequests>({
@@ -18,18 +20,40 @@ export default function Requests() {
             <>
                 <h1>Solicitações de contratação</h1>
                 {requests.map(request => (
-                    <div>
-                        <h3>{request.usuarioComum.nome}</h3>
-                        <div className="flex items-center gap-2">
-                            <Calendar />
-                            <p>
-                                Solicitado em{" "}
-                                {new Date(
-                                    request.dataContratacao,
-                                ).toLocaleDateString("pt-br")}
-                            </p>
-                        </div>
-                    </div>
+                    <Card className="w-64 text-center">
+                        <CardHeader>
+                            <CardTitle>{request.usuarioComum.nome}</CardTitle>
+                        </CardHeader>
+                        <CardContent className="space-y-2">
+                            <div className="flex items-center gap-2">
+                                <CalendarDays />
+                                <p className="text-sm font-medium">
+                                    Solicitado em{" "}
+                                    {new Date(
+                                        request.dataContratacao,
+                                    ).toLocaleDateString("pt-br")}
+                                </p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <CalendarX />
+                                <p className="text-sm font-medium">
+                                    Válido até{" "}
+                                    {new Date(
+                                        request.prazoContratacao,
+                                    ).toLocaleDateString("pt-br")}
+                                </p>
+                            </div>
+                            <div className="flex w-full items-center gap-2">
+                                <Button className="flex-1">Aceitar</Button>
+                                <Button
+                                    className="flex-1"
+                                    variant="destructive"
+                                >
+                                    Recusar
+                                </Button>
+                            </div>
+                        </CardContent>
+                    </Card>
                 ))}
             </>
         )
