97de1bb5a395c66f3ffc5863291346fff5f1acf8
m4c1elz <milaelipe.maciel@gmail.com>
2024-12-09 13:15:32 -0300

    feat: add user info in settings page

--- client/src/pages/options/index.tsx
+++ client/src/pages/options/index.tsx
@@ -1,11 +1,12 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { AppTabs } from "@/components/app-tabs"
 import { Button } from "@/components/ui/button"
+import { isUserCommon } from "@/helpers/is-user-common"
 import { useAuth } from "@/providers/auth-provider"
 import { useNavigate } from "@/router"
 
 export default function Options() {
-    const { logoutMutation } = useAuth()
+    const { logoutMutation, user } = useAuth()
     const navigate = useNavigate()
 
     async function handleLogout() {
@@ -19,7 +20,21 @@ export default function Options() {
             <AppTabs />
             <div className="mx-auto my-12 h-screen w-11/12 space-y-4 overflow-auto pb-16 md:w-3/5">
                 <h3>Opções</h3>
-                <section className="space-y-2">
+                <section className="bg-primary space-y-2 rounded-md border px-6 py-4">
+                    <h5>Usuário</h5>
+                    <h3>{user?.nome}</h3>
+                    <p>
+                        E-mail:{" "}
+                        <span className="font-medium">{user?.email}</span>
+                    </p>
+                    <p>
+                        Tipo de usuário:{" "}
+                        <span className="font-medium">
+                            {isUserCommon(user) ? "Comum" : "Profissional"}
+                        </span>
+                    </p>
+                </section>
+                <section className="bg-primary space-y-2 rounded-md border px-6 py-4">
                     <h5>Terminar sessão</h5>
                     <p className="text-foreground/50">
                         Deslogar da sessão atual.
